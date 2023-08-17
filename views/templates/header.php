8907c9d8fe8902413291f1f1fb1b086ce65f32ed
Administrador <ivan.24.burgio@gmail.com>
2023-08-17 09:23:16 -0300

    Añadiendo el html al header

---
+++ views/templates/header.php
@@ -0,0 +1,21 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar sesión</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">
+                    &#60;DevWebCamp/>
+                </h1>
+            </a>
+
+            <p class="header__texto">Octubre 5-6 2023</p>
+            <p class="header__texto header__texto--modalidad">En línea - Presencial</p>
+
+            <a href="/registro" class="header__boton">Comprar Pase</a>
+        </div>
+    </div>
+</header>
