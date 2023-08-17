da3e35a88925cc9c1209b1dd28f8fd6ec4cf6ffe
Administrador <ivan.24.burgio@gmail.com>
2023-08-17 16:13:23 -0300

    Creando una barra de navegacion

--- views/templates/header.php
+++ views/templates/header.php
@@ -7,9 +7,7 @@
 
         <div class="header__contenido">
             <a href="/">
-                <h1 class="header__logo">
-                    &#60;DevWebCamp/>
-                </h1>
+                <h1 class="header__logo">&#60;DevWebCamp/></h1>
             </a>
 
             <p class="header__texto">Octubre 5-6 2023</p>
@@ -19,3 +17,20 @@
         </div>
     </div>
 </header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">
+                &#60;DevWebCamp/>
+            </h2>
+        </a>
+
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
