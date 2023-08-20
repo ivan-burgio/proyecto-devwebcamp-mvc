fbac8f87195f97d58fcfaec8918c5bea77686171
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-20 11:36:47 -0300

    Resaltar la página actual

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,34 +1,34 @@
 <aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
-        <a href="/admin/dashboard" class="dashboard__enlace">
+        <a href="/admin/dashboard" class="dashboard__enlace <?php echo pagina_actual('/dashboard') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-house" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Inicio
             </span>
         </a>
 
-        <a href="/admin/ponentes" class="dashboard__enlace">
+        <a href="/admin/ponentes" class="dashboard__enlace <?php echo pagina_actual('/ponentes') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-microphone" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Ponentes
             </span>
         </a>
 
-        <a href="/admin/eventos" class="dashboard__enlace">
+        <a href="/admin/eventos" class="dashboard__enlace <?php echo pagina_actual('/eventos') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-calendar-days" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Eventos
             </span>
         </a>
 
-        <a href="/admin/registrados" class="dashboard__enlace">
+        <a href="/admin/registrados" class="dashboard__enlace <?php echo pagina_actual('/registrados') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-users" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Registrados
             </span>
         </a>
 
-        <a href="/admin/regalos" class="dashboard__enlace">
+        <a href="/admin/regalos" class="dashboard__enlace <?php echo pagina_actual('/regalos') ? 'dashboard__enlace--actual' : ''; ?>">
             <i class="fa-solid fa-gift" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Regalos
