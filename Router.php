7a61ee2a60904b4ab6533ae86c2cc08946f98d8a
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-20 09:55:16 -0300

    Creando el layout para admin

--- Router.php
+++ Router.php
@@ -48,6 +48,14 @@ class Router
 
         $contenido = ob_get_clean(); // Limpia el Buffer
 
-        include_once __DIR__ . '/views/layout.php';
+        // Utilizar el Layout de acuerdo a la url
+        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+
+        if(str_contains($url_actual, '/admin')) {
+            include_once __DIR__ . '/views/admin-layout.php';
+        } else {
+            include_once __DIR__ . '/views/layout.php';
+        }
+
     }
 }
