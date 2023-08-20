a455bf56a4c51de8eb2dd36bc947e9d261fa2495
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-20 10:22:20 -0300

    Creando los enlaces para el panel de administración

---
+++ views/templates/admin-sidebar.php
@@ -0,0 +1,38 @@
+<aside class="dahsboard__sidebar">
+    <nav class="dashboard__menu">
+        <a href="/admin/dashboard" class="dashboard__enlace">
+            <i class="fa-solid fa-house"></i>
+            <span class="dahsboard__menu-texto">
+                Inicio
+            </span>
+        </a>
+
+        <a href="/admin/ponentes" class="dashboard__enlace">
+            <i class="fa-solid fa-microphone"></i>
+            <span class="dahsboard__menu-texto">
+                Ponentes
+            </span>
+        </a>
+
+        <a href="/admin/eventos" class="dashboard__enlace">
+            <i class="fa-solid fa-calendar-days"></i>
+            <span class="dahsboard__menu-texto">
+                Eventos
+            </span>
+        </a>
+
+        <a href="/admin/registrados" class="dashboard__enlace">
+            <i class="fa-solid fa-users"></i>
+            <span class="dahsboard__menu-texto">
+                Registrados
+            </span>
+        </a>
+
+        <a href="/admin/regalos" class="dashboard__enlace">
+            <i class="fa-solid fa-gift"></i>
+            <span class="dahsboard__menu-texto">
+                Regalos
+            </span>
+        </a>
+    </nav>
+</aside>
