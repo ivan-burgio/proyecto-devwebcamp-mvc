8242e9bcf41150e5bd5fc69f67d1b8ffdbec1650
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-20 11:00:59 -0300

    CSS al panel de administracion

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,35 +1,35 @@
-<aside class="dahsboard__sidebar">
+<aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
         <a href="/admin/dashboard" class="dashboard__enlace">
-            <i class="fa-solid fa-house"></i>
+            <i class="fa-solid fa-house" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Inicio
             </span>
         </a>
 
         <a href="/admin/ponentes" class="dashboard__enlace">
-            <i class="fa-solid fa-microphone"></i>
+            <i class="fa-solid fa-microphone" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Ponentes
             </span>
         </a>
 
         <a href="/admin/eventos" class="dashboard__enlace">
-            <i class="fa-solid fa-calendar-days"></i>
+            <i class="fa-solid fa-calendar-days" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Eventos
             </span>
         </a>
 
         <a href="/admin/registrados" class="dashboard__enlace">
-            <i class="fa-solid fa-users"></i>
+            <i class="fa-solid fa-users" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Registrados
             </span>
         </a>
 
         <a href="/admin/regalos" class="dashboard__enlace">
-            <i class="fa-solid fa-gift"></i>
+            <i class="fa-solid fa-gift" class="dashboard__icono"></i>
             <span class="dahsboard__menu-texto">
                 Regalos
             </span>
