bfc37ebaaefe4da6c23904fd0c45bd4aed848602
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-25 12:55:03 -0300

    Listar y eliminar tags

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,7 +1,9 @@
-(function() {
+(function() { // IFFI
     const tagsInput = document.querySelector('#tags_input');
 
     if(tagsInput) {
+        const tagsDiv = document.querySelector('#tags');
+        const tagsInputHidden = document.querySelector('[name="tags"]');
         let tags = [];
 
         // Escuchar los cambios en el input
@@ -12,13 +14,35 @@
                 if(e.target.value.trim() === '' || e.target.velue < 1) {
                     return;
                 }
-                
+
                 e.preventDefault();
                 tags = [...tags, e.target.value.trim()];
                 tagsInput.value = '';
-
-                console.log(tags)
+                mostrarTags();
             }
+        }
+
+        function mostrarTags() {
+            tagsDiv.textContent = '';
+            tags.forEach(tag => {
+                const etiqueta = document.createElement('LI');
+                etiqueta.classList.add('formulario__tag');
+                etiqueta.textContent = tag;
+                etiqueta.ondblclick = eliminarTag;
+                tagsDiv.appendChild(etiqueta);
+            })
+
+            actualizarInputHidden();
+        }
+
+        function eliminarTag(e) {
+            e.target.remove();
+            tags = tags.filter(tag => tag != e.target.textContent);
+            actualizarInputHidden();
+        }
+
+        function actualizarInputHidden() {
+            tagsInputHidden.value = tags.toString();
         }
     }
 })()
