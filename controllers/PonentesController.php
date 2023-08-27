e053e24fc44c2ac23e9edcff41861546fc91257e
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-27 16:07:33 -0300

    Leer la iagen del ponente y generar varsiones png y webp

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Ponente;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController {
 
@@ -21,6 +22,23 @@ class PonentesController {
         $ponente = new Ponente;
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Leer image
+            if(!empty($_FILES['imagen']['tmp_name'])) {
+                $carpeta_imagenes = '../public/img/speakers';
+
+                // Crear la carpeta si no existe
+                if(!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0755, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
+
+                $nombre_imagen = md5(uniqid(rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            }
+
             $ponente->sincronizar($_POST);
 
             // Validar
