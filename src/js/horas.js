fa721852b0e386fcfe53966e54def78a46c5b3cc
Administrador <ivan.24.burgio@gmail.com>
2023-09-07 19:31:38 -0300

    Deshabilitar visualmente las horas tomadas

--- src/js/horas.js
+++ src/js/horas.js
@@ -39,9 +39,13 @@
             // Comprobar eventos ya tomados y quitar la variable de deshabilitado
             const horasTomadas = eventos.map(evento => evento.hora_id);
             const listadoHoras = document.querySelectorAll('#horas li');
-            const resultado = listadoHoras.filter(li => horasTomadas.includes(li.dataset.horaId));
 
-            const horasDisponibles = document.querySelectorAll('#horas li');
+            const listadoHorasArray = Array.from(listadoHoras);
+
+            const resultado = listadoHorasArray.filter(li => !horasTomadas.includes(li.dataset.horaId));
+            resultado.forEach(li => li.classList.remove('horas__hora--deshabilitada'));
+
+            const horasDisponibles = document.querySelectorAll('#horas li:not(.horas__hora--deshabilitada)');
             horasDisponibles.forEach(hora => hora.addEventListener('click', seleccionarHora));
         }
 
