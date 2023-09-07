d46134eece3716ce48984e88b950295459574339
Administrador <ivan.24.burgio@gmail.com>
2023-09-07 13:40:58 -0300

    Revisar con una consulta dia y hora para ver las horas disponibles

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\APIEventos;
 use Controllers\AuthController;
 use Controllers\EventosController;
 use Controllers\RegalosController;
@@ -47,6 +48,8 @@ $router->post('/admin/ponentes/eliminar', [PonentesController::class, 'eliminar'
 $router->get('/admin/eventos', [EventosController::class, 'index']);
 $router->get('/admin/eventos/crear', [EventosController::class, 'crear']);
 $router->post('/admin/eventos/crear', [EventosController::class, 'crear']);
+
+$router->get('/api/eventos-horarios', [APIEventos::class, 'index']);
 
 $router->get('/admin/registrados', [RegistradosController::class, 'index']);
 
