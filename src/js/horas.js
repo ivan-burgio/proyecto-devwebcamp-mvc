a8b2dabdc5fc88848c8faa0ad8ee170e2fcf96af
Administrador <ivan.24.burgio@gmail.com>
2023-09-07 16:32:54 -0300

    Resaltar visualmente cuando una hora es seleccionada

--- src/js/horas.js
+++ src/js/horas.js
@@ -41,6 +41,16 @@
         }
 
         function seleccionarHora(e) {
+            // Deshabilitar la hora precia si hay un nuevo click
+            const horaPrevia = document.querySelector('.horas__hora--seleccionada');
+            
+            if(horaPrevia) {
+                horaPrevia.classList.remove('horas__hora--seleccionada');
+            }
+
+            // Agregar clase de seleccionado
+            e.target.classList.add('horas__hora--seleccionada');
+
             inputHiddenHora.value = e.target.dataset.horaId;
         }
     }
