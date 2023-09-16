c5ce87959a1133491bb3e3e79c498fd181c8e91a
ivan-burgio <ivan.24.burgio@gmail.com>
2023-09-16 11:47:22 -0300

    Avance estilos

--- views/paginas/devwebcamp.php
+++ views/paginas/devwebcamp.php
@@ -3,7 +3,7 @@
     <p class="devwebcamp__descripcion">Conoce la conferencia más importante de Latinoamérica</p>
 
     <div class="devwebcamp__grid">
-        <div <?php aos_animacion(); ?> class="devwebcamp__imagen">
+        <div <?php // aos_animacion(); ?> class="devwebcamp__imagen">
             <picture>
                 <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
                 <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
@@ -12,9 +12,9 @@
         </div>
 
         <div  class="devwebcamp__contenido">
-            <p <?php aos_animacion(); ?> class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
+            <p <?php // aos_animacion(); ?> class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
             
-            <p <?php aos_animacion(); ?> class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
+            <p <?php // aos_animacion(); ?> class="devwebcamp__texto">Aliquam est elit, condimentum a justo vitae, fringilla dignissim tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam quis lorem eget felis dignissim dignissim. Praesent ac lorem convallis, ultrices neque quis, cursus lorem.</p>
         </div>
     </div>
 </main>
